253d8f6011265d2617dcf48a59c836c69498af27
Furkan <ozdemirf97@icloud.com>
2024-01-01 22:16:37 -0500

    v1

--- script.js
+++ script.js
@@ -111,19 +111,19 @@ const game = {
 
 // 1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
 
-for (const [i, player] of game.scored.entries())
-  console.log(`Goal ${i + 1} : ${player}`);
+// for (const [i, player] of game.scored.entries())
+//   console.log(`Goal ${i + 1} : ${player}`);
 
-//2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+// //2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
 
-let sum = 0;
-const values = Object.values(game.odds);
+// let sum = 0;
+// const values = Object.values(game.odds);
 
-for (const value of values) {
-  sum += value;
-}
-sum /= values.length;
-console.log(sum);
+// for (const value of values) {
+//   sum += value;
+// }
+// sum /= values.length;
+// console.log(sum);
 
 // 3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
 //       Odd of victory Bayern Munich: 1.33
@@ -131,10 +131,10 @@ console.log(sum);
 //       Odd of victory Borrussia Dortmund: 6.5
 // Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
 
-for (const [team, odd] of Object.entries(game.odds)) {
-  const teamStr = team === 'x' ? 'draw' : `Victory ${game[team]}`;
-  console.log(`Odd of Victory ${teamStr}: ${odd}`);
-}
+// for (const [team, odd] of Object.entries(game.odds)) {
+//   const teamStr = team === 'x' ? 'draw' : `Victory ${game[team]}`;
+//   console.log(`Odd of Victory ${teamStr}: ${odd}`);
+//}
 
 // BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
 //       {
@@ -143,9 +143,47 @@ for (const [team, odd] of Object.entries(game.odds)) {
 //         Lewandowski: 2
 //       }
 
-const scorers = {};
+// const scorers = {};
 
-for (const player of game.scored) {
-  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+// for (const player of game.scored) {
+//   scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+// }
+// console.log(scorers);
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+// 1. Create an array 'events' of the different game events that happened (no duplicates)
+
+// set
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+
+console.log(`An event happened every ${90 / gameEvents.size} minutes `);
+
+// 4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+//       [FIRST HALF] 17: ⚽️ GOAL
+
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'First' : 'Second';
+  console.log(`[${half} Half] ${min}: ${event}`);
 }
-console.log(scorers);
+
+// */
